41d079f9d1ac6681e0b9a92897a3e3fb5f6274dc
Melanie Faibish <mlf398@nyu.edu>
2019-06-04 22:53:16 -0400

    User inputs

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ create new repo called rock-paper-scissors-exercise
 setup game.py file to print Rock, Paper, Scissors, Shoot!
 
 Setup conda environment and install pytest package 
+
+#Inputs
+user is prompted to select rock, paper or scissors
+
+system will report results of the game
--- game.py
+++ game.py
@@ -1,3 +1,34 @@
 #game .py
 
+import random
+
+print("-----------------------------")
 print("Rock, Paper, Scissors, Shoot!")
+print("-----------------------------")
+
+options = ["rock", "paper", "scissors"]
+
+choice = input("Let's play a game! Select rock, paper or scissors: ") #user input
+
+if choice in options:
+    print("-----------------------------")
+    print ("You chose:", choice)   
+else:
+    raise ValueError("OOPS - please type 'rock', 'paper' or 'scissors'.") #error checking
+
+computer = random.choice(options) #computer selects option
+print("The computer chose:", computer)
+print("-----------------------------")
+
+if choice == "rock" and computer == "scissors":
+    print("You win!")
+elif choice == "paper" and computer == "rock":
+    print ("You win!")
+elif choice == "scissors" and computer == "paper":
+    print("You win!")
+elif choice == computer:
+    print("It's a tie")
+    
+print("-----------------------------")
+print("Thanks for playing! Please play again")
+
